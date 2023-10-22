85088efd06f206c91ac6590753c8d87bef0b9b65
jamitz440 <jamster44085@gmail.com>
2023-10-22 20:46:48 +0100

    Start of the portfolio website

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,121 @@
-import Image from 'next/image'
+"use client";
+import { Dispatch, SetStateAction, useState } from "react";
+import { FaStarOfLife } from "react-icons/fa";
 
-export default function Home() {
+export default function App() {
+  const [page, setPage] = useState("Home");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="w-screen ">
+      <NavBar page={page} setPage={setPage} />
+      <div className=" items-center flex h-screen">
+        <Home />
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className=" items-center flex h-screen">
+        <Card />
       </div>
     </main>
-  )
+  );
 }
+
+const Card = () => {
+  return (
+    <div className=" bg-card h-auto w-screen rounded-[50px] mx-16 -translate-y-32 grid grid-cols-3 gap-4">
+      <div className="items-center  col-span-2 m-8">
+        <div className="text-nav font-bold text-3xl text-primary">
+          A little bit about me
+        </div>
+        <div className="mt-4 text-white font-nav text-xl">
+          I began my career in the RAF as an avionics engineer. However, it was
+          during my off-hours, tinkering with game creation in Unity3D, that I
+          discovered a burgeoning passion for coding. This newfound enthusiasm
+          led me to a coveted coding role within the Air Force, where I
+          sharpened my skills with Python. Outside of my official duties, my
+          insatiable curiosity drove me to explore Django and later, React. For
+          me, every coding challenge presents a puzzle, and I revel in piecing
+          together its solution.
+          <br />
+          <br /> When I'm not immersed in lines of code, you can find me
+          engrossed in the world of 3D printing. <br />
+          <br />
+          As for the future? My aspirations are clear-cut: I'm on a quest to
+          cement my position in the software development realm, whether that
+          means building robust applications or designing web wonders. The
+          journey so far has been a whirlwind of excitement, and I eagerly await
+          the countless lines of code that lie ahead.
+        </div>
+      </div>
+      <div className="items-center col-start-3 text-center  bg-white rounded-[40px] m-8 aspect-square">
+        Avatar Placeholder
+      </div>
+    </div>
+  );
+};
+
+interface NavBar {
+  page: string;
+  setPage: Dispatch<SetStateAction<string>>;
+}
+const NavBar = ({ page, setPage }: NavBar) => {
+  function handleOnClick(e: any) {
+    setPage(e.target.innerHTML);
+  }
+  return (
+    <div className="h-32 text-2xl flex items-center sticky top-0 w-full justify-around font-bold text-white bg-gradient-to-b from-background  to-transparent z-10">
+      <h1
+        onClick={handleOnClick}
+        className={`${
+          page === "Home" ? "underline" : ""
+        } hover:underline underline-offset-4 decoration-primary decoration-2 hover:cursor-pointer`}
+      >
+        Home
+      </h1>
+      <h1
+        onClick={handleOnClick}
+        className={`${
+          page === "About" ? "underline" : ""
+        } hover:underline underline-offset-4 decoration-primary decoration-2 hover:cursor-pointer`}
+      >
+        About
+      </h1>
+      <span className="text-primary text-6xl">
+        <FaStarOfLife />
+      </span>
+      <h1
+        onClick={handleOnClick}
+        className={`${
+          page === "Work" ? "underline" : ""
+        } hover:underline underline-offset-4 decoration-primary decoration-2 hover:cursor-pointer`}
+      >
+        Work
+      </h1>
+      <h1
+        onClick={handleOnClick}
+        className={`${
+          page === "Contact" ? "underline" : ""
+        } hover:underline underline-offset-4 decoration-primary decoration-2 hover:cursor-pointer`}
+      >
+        Contact
+      </h1>
+    </div>
+  );
+};
+
+const Home = () => {
+  return (
+    <div className="w-screen flex items-center">
+      <div className="font-nav font-black text-white -translate-y-32 ml-32">
+        <div className="text-8xl">
+          <h1>Hello,</h1>
+          <h1>my name is James</h1>
+        </div>
+        <div className="mt-4 font-semibold text-xl">
+          <h1>
+            Im a software engineer based in the UK. I have worked with PBAPro,
+            Django and React frameworks
+          </h1>
+          <h1>Using languages such as Python and TypeScript.</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "#212121",
+        primary: "#DDBF53",
+        card: "#2A2A2A",
+      },
+      fontFamily: {
+        nav: ["Roboto", "serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
